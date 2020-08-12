943fa89866713e91637da4c7926809ba80454f55
jadibrahim99 <jadintx@gmail.com>
2020-08-12 14:49:06 -0500

    fixing conflicts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-=======
->>>>>>> aefd65d191b852faeb88d6bd6b3404d41610b762
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
@@ -15,10 +9,3 @@ ReactDOM.render(
     document.getElementById('root'),
 );
 
-<<<<<<< HEAD
-
-=======
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
->>>>>>> aefd65d191b852faeb88d6bd6b3404d41610b762
